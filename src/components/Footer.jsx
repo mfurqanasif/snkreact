41aad940f4f527f22f98290fa8fc4dd87539abf3
Muhammad Furqan Asif <45694268+mfurqanasif@users.noreply.github.com>
2025-06-13 15:16:50 +0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -148,7 +148,7 @@ export default function Footer() {
                     onMouseOver={e => e.target.style.color = "#0d47a1"}
                     onMouseOut={e => e.target.style.color = "#1976d2"}
                   >
-                    Developed by PropPropVenture Softwares
+                    Developed by PropVenture Softwares
                   </span>
                 </p>
               </div>
